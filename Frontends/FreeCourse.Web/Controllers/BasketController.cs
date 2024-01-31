24a06fbf39398e15e839fc3b6790bb2a10424187
xeneskus <xeneskus@gmail.com>
2024-01-31 22:33:34 +0300

    Added BasketController Index.cshml

--- Frontends/FreeCourse.Web/Controllers/BasketController.cs
+++ Frontends/FreeCourse.Web/Controllers/BasketController.cs
@@ -1,12 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FreeCourse.Web.Models.Baskets;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FreeCourse.Web.Controllers
 {
+    [Authorize]
     public class BasketController : Controller
     {
-        public IActionResult Index()
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+
+        public BasketController(ICatalogService catalogService, IBasketService basketService)
         {
-            return View();
+            _catalogService = catalogService;
+            _basketService = basketService;
         }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _basketService.Get());
+        }
+
+        public async Task<IActionResult> AddBasketItem(string courseId)
+        {
+            var course = await _catalogService.GetByCourseId(courseId);
+
+            var basketItem = new BasketItemViewModel { CourseId = course.Id, CourseName = course.Name, Price = course.Price };
+
+            await _basketService.AddBasketItem(basketItem);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> RemoveBasketItem(string courseId)
+        {
+            var result = await _basketService.RemoveBasketItem(courseId);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //public async Task<IActionResult> ApplyDiscount(DiscountApplyInput discountApplyInput)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        TempData["discountError"] = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).First();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    var discountStatus = await _basketService.ApplyDiscount(discountApplyInput.Code);
+
+        //    TempData["discountStatus"] = discountStatus;
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+        public async Task<IActionResult> CancelApplyDiscount()
+        {
+            await _basketService.CancelApplyDiscount();
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
