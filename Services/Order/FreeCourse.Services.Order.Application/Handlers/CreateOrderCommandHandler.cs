dc6f9de5fd7fe89f71dd89946da4f4d7e0e70dd6
xeneskus <xeneskus@gmail.com>
2024-02-05 13:06:32 +0300

    Added OrderController/Checkout.cshtml for mvc

--- Services/Order/FreeCourse.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
+++ Services/Order/FreeCourse.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -8,35 +8,35 @@ using MediatR;
 namespace FreeCourse.Services.Order.Application.Handlers
 {
     public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Response<CreatedOrderDto>>
+    {
+        private readonly OrderDbContext _context;
+
+        public CreateOrderCommandHandler(OrderDbContext context)
         {
-            private readonly OrderDbContext _context;
+            _context = context;
+        }
 
-            public CreateOrderCommandHandler(OrderDbContext context)
+        public async Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var newAddress = new Address(request.Address.Province, request.Address.District, request.Address.Street, request.Address.ZipCode, request.Address.Line);
+
+            Domain.OrderAggregate.Order newOrder = new Domain.OrderAggregate.Order(request.BuyerId, newAddress);
+
+            request.OrderItems.ForEach(x =>
             {
-                _context = context;
-            }
 
-            public async Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
-            {
-                var newAddress = new Address(request.Address.Province, request.Address.District, request.Address.Street, request.Address.ZipCode, request.Address.Line);
+                newOrder.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
+            });
 
-                Domain.OrderAggregate.Order newOrder = new Domain.OrderAggregate.Order(request.BuyerId, newAddress);
-
-                request.OrderItems.ForEach(x =>
-                {
-
-                    newOrder.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
-                });
-
-               await _context.Orders.AddAsync(newOrder);
-               await  _context.SaveChangesAsync();
-                return Response<CreatedOrderDto>.Success(new CreatedOrderDto { OrderId=newOrder.Id},200);
-
-            }
+            await _context.Orders.AddAsync(newOrder);
+            await _context.SaveChangesAsync();
+            return Response<CreatedOrderDto>.Success(new CreatedOrderDto { OrderId = newOrder.Id }, 200);
 
         }
 
     }
+
+}
 
 
 
